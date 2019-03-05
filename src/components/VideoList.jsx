a97ce928212a65aabbcc70568d5d5c816b4f2065
harrisonywu <harrisonywu@gmail.com>
2019-03-04 21:12:00 -0800

    Render App correctly

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,11 +1,11 @@
 
-import exampleVideoData from '../data/exampleVideoData.js'
+import exampleVideoData from '../data/exampleVideoData.js';
 
 var VideoList = (props) => (
   <div className="video-list">
     {props.exampleVideoData.map(video => 
       <VideoListEntry video={video}/>
-      )}
+    )}
     {/* <div><h5><em>videoListEntry</em> view goes here</h5></div>
     <div><h5><em>videoListEntry</em> view goes here</h5></div>
     <div><h5><em>videoListEntry</em> view goes here</h5></div>
