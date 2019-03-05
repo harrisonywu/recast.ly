5dc591620e1c96c030f65c99eba10382e32de596
harrisonywu <harrisonywu@gmail.com>
2019-03-04 21:11:14 -0800

    Render App correctly

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,13 +1,20 @@
-var VideoList = () => (
+
+import exampleVideoData from '../data/exampleVideoData.js'
+
+var VideoList = (props) => (
   <div className="video-list">
+    {props.exampleVideoData.map(video => 
+      <VideoListEntry video={video}/>
+      )}
+    {/* <div><h5><em>videoListEntry</em> view goes here</h5></div>
     <div><h5><em>videoListEntry</em> view goes here</h5></div>
     <div><h5><em>videoListEntry</em> view goes here</h5></div>
     <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
+    <div><h5><em>videoListEntry</em> view goes here</h5></div> */}
   </div>
 );
 
+console.log(exampleVideoData);
 // PropTypes tell other developers what `props` a component expects
 // Warnings will be shown in the console when the defined rules are violated
 VideoList.propTypes = {
@@ -17,3 +24,12 @@ VideoList.propTypes = {
 // In the ES6 spec, files are "modules" and do not share a top-level scope.
 // `var` declarations will only exist globally where explicitly defined.
 export default VideoList;
+
+
+//Refactor the videolist to dynamic rendering
+//INPUTS: array of objects (videos)
+//OUTPUTS: each video in its own html tag
+// no constraints or edge cases
+
+//justification: take mock data and render each one individually (as a video list entry), and attach them to our video list
+// explanation: we'll take our array of objects and do a forEach/for loop/map on each object and send it to video list entry.
